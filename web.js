6b1ba372cac3f1ffbd2721a2864fa95ce19c65e9
Junaidul <69438856+Junaidul@users.noreply.github.com>
2023-10-19 23:41:41 -0400

    Update web.js

--- web.js
+++ web.js
@@ -9,4 +9,25 @@ textarea.addEventListener("keyup" , (e) => {
 
 
 
+//This is for the type writer effect
+const h1 = document.querySelector('#buy');
+const text = "Buy Your Dream Car With Us!";
+
+
+function typeWriter(e , text , i = 0){
+    if (i === 0){
+        h1.textContent = '';
+    }
+    e.textContent += text[i];
+    if (i === text.length - 1){
+        return;
+    }
+    setTimeout(() => typeWriter(e , text , i + 1) , 200)
+}
+
+
+
+typeWriter(h1 , text)
+
+
 
